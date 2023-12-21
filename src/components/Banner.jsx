f5af0cf73fbdb0f51d4b28a46289b5fed4ce2b4c
SiamIslamSagor <msiamislam12345@gmail.com>
2023-12-21 23:38:10 +0600

    create a responsive relavant banner and show it in home page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
+import bannerBg from "../assets/taskManagerBannerImg.jpg";
 const Banner = () => {
+  const bannerBgStyle = {
+    backgroundImage: `url(${bannerBg})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
+
   return (
-    <div className="flex flex-col item-center justify-center border p-5 my-10">
-      <h1 className="text-4xl text-center font-bold">Banner</h1>
+    <div
+      style={bannerBgStyle}
+      className=" relative h-[60vh] sm:h-[70vh] md:h-[700px] lg:h-[750px] 2xl:h-[820px] text-white "
+    >
+      <div
+        className="flex flex-col h-full justify-center items-center bg-black bg-opacity-60 max-md:px-5
+      "
+      >
+        <h1 className="text-xl md:text-3xl lg:text-4xl 2xl:text-5xl text-center font-bold">
+          Smart Task Management
+          <br />
+          Elevated Experience
+        </h1>
+        <p className="text-center md:text-lg xl:text-xl 2xl:text-2xl max-w-3xl xl:max-w-4xl 2xl:max-w-5xl mt-2">
+          Say goodbye to chaos and hello to clarity! Experience the simplicity
+          of tracking, prioritizing, and completing tasks effortlessly, all
+          within our powerful task management interface.
+        </p>
+
+        <Link to={"/auth/login"}>
+          <button className="btn btn-neutral max-sm:btn-sm md:text-lg text-lp-color bg-ds-color mt-5 lg:mt-8">
+            Let’s Explore
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
