9dfa866a593be073d218b57884fb2fe65379f034
SiamIslamSagor <msiamislam12345@gmail.com>
2023-12-22 01:23:37 +0600

    create authProvider with all auth management function and also create privet or protected routee

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,101 @@
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import auth from "../config/firebase.config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  // state
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [userLoading, setUserLoading] = useState(true);
+
+  /// google Provider
+  const googleProvider = new GoogleAuthProvider();
+
+  //   create user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   login user
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  /// google user login
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // sign out user
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  // update user
+  const updateUserData = (userName, userPhotoUrl) => {
+    return updateProfile(auth.currentUser, {
+      displayName: userName,
+      photoURL: userPhotoUrl,
+    });
+  };
+
+  // go to top
+  const gooTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  // auth observer
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, presentUser => {
+      //set the user in state
+      setUser(presentUser);
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, [loading, user?.email]);
+
+  const data = {
+    user,
+    setLoading,
+    loading,
+    setUserLoading,
+    userLoading,
+    createUser,
+    logIn,
+    googleLogin,
+    logOut,
+    updateUserData,
+    gooTop,
+  };
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthContext.Provider value={data}>{children}</AuthContext.Provider>
+    </QueryClientProvider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
---
+++ src/routes/PrivetRoute.jsx
@@ -0,0 +1,9 @@
+const PrivetRoute = () => {
+  return (
+    <div className="flex flex-col item-center justify-center border p-5 my-10">
+      <h1 className="text-4xl text-center font-bold">PrivetRoute</h1>
+    </div>
+  );
+};
+
+export default PrivetRoute;
